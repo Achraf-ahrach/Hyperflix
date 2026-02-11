63b0333e33205553b78439929d5d61b25638d3d6
elyassir <yassir.elmassafi@gmail.com>
2026-02-11 16:25:40 +0100

    chore: remove output configuration from next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true, // Static export doesn't support the default Image Optimization API
+    unoptimized: true,
   },
 };
 
