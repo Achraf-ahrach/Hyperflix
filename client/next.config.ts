13116c7b60986b7e4359240d57db40db72e21e84
elyassir <yassir.elmassafi@gmail.com>
2026-02-11 16:33:13 +0100

    refactor: reorganize Dockerfile stages and update next.config.ts for image optimization

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,38 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yts.lt',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yts.bz',
+        pathname: '/**',
+      }
+    ],
   },
 };
 
