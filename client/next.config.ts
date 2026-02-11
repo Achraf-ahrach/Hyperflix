523da9c5982aed7199902737507cb1e93a386378
elyassir <yassir.elmassafi@gmail.com>
2026-02-11 15:20:44 +0100

    refactor Dockerfile and next.config.ts for static export; remove unused images

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,39 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-        pathname: '/t/p/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'yts.lt',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'yts.bz',
-        pathname: '/**',
-      }
-    ],
+    unoptimized: true, // Static export doesn't support the default Image Optimization API
   },
 };
 
